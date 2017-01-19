46e4ca74f8d1b2a0eb1381ac685ce29a64f05ace
xieyuqing <451658995@qq.com>
2017-01-19 12:49:49 +0800

    收藏宝贝列表的adapter

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/adapter/CollectAdapter.java
@@ -0,0 +1,121 @@
+package cn.ucai.fulicenter.controller.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.CollectBean;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.view.FooterViewHolder;
+import cn.ucai.fulicenter.view.MFGT;
+
+/**
+ * Created by Administrator on 2017/1/11 0011.
+ */
+
+public class CollectAdapter extends RecyclerView.Adapter {
+    Context mContext;
+    ArrayList<CollectBean> mList;
+    boolean isMore;
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    public CollectAdapter(Context context, ArrayList<CollectBean> list) {
+        mContext = context;
+        mList = new ArrayList<>();
+        mList.addAll(list);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new FooterViewHolder(View.inflate(mContext, R.layout.item_footer, null));
+        } else {
+            holder = new CollectViewHolder(View.inflate(mContext, R.layout.item_collect, null));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        if (getItemViewType(position) == I.TYPE_FOOTER) {
+            FooterViewHolder vh = (FooterViewHolder) holder;
+            vh.setFooterString(mContext.getString(getFooterString()));
+        } else {
+            CollectViewHolder vh = (CollectViewHolder) holder;
+            ImageLoader.downloadImg(mContext, vh.mIvGoodsThumb, mList.get(position).getGoodsThumb());
+            vh.mTvGoodsName.setText(mList.get(position).getGoodsName());
+            vh.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MFGT.gotoGoodsDetail(mContext, mList.get(position).getGoodsId());
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public void initData(ArrayList<CollectBean> list) {
+        if (mList != null) {
+            mList.clear();
+        }
+        addData(list);
+    }
+
+    public void addData(ArrayList<CollectBean> list) {
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public int getFooterString() {
+
+        return isMore ? R.string.load_more : R.string.no_more;
+    }
+
+    static class CollectViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.ivGoodsThumb)
+        ImageView mIvGoodsThumb;
+        @BindView(R.id.tvGoodsName)
+        TextView mTvGoodsName;
+        @BindView(R.id.iv_collect_del)
+        ImageView mIvCollectDel;
+        @BindView(R.id.layout_goods)
+        RelativeLayout mLayoutGoods;
+
+        CollectViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
